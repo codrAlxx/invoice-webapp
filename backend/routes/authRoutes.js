b4be9da33c1ba582243c7e885656069ac63e3976
codealxx <alok02jha@gmail.com>
2023-06-19 11:29:41 +0530

    Feat: Implement Access and Refresh Token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,14 +3,17 @@ import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
+import newAccessToken from "../controllers/auth/refreshTokenController.js";
 
 const router = express.Router();
 
-console.log("In auth routes")
 router.post("/register", registerUser);
 
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
+
 router.post("/login", loginLimiter, loginUser);
+
+router.get("/new_access_token", newAccessToken);
 
 router.get("/authTest" , (req, res)=> {
     // console.log(req.url)
