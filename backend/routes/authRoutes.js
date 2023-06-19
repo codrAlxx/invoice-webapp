4d83045a0400526a1ddfb67f94486bb788351bb1
codealxx <alok02jha@gmail.com>
2023-06-19 11:08:32 +0530

    Feat: Login User

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
+import loginUser from "../controllers/auth/loginController.js";
+import { loginLimiter } from "../middleware/apiLimiter.js";
 
 const router = express.Router();
 
@@ -8,6 +10,7 @@ console.log("In auth routes")
 router.post("/register", registerUser);
 
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
+router.post("/login", loginLimiter, loginUser);
 
 router.get("/authTest" , (req, res)=> {
     // console.log(req.url)
