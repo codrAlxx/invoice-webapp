7042991640dd8e5e90d7780fb74b061c6bf8cfe7
codealxx <alok02jha@gmail.com>
2023-06-20 11:18:46 +0530

    Created Common Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div>
       <h1>Welcome to the Mern Invoice App</h1>
+      <Footer />
     </div>
   )
 }
