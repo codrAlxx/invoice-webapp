b2fe9b48c5a620235446e6d8b274bb46c80f94dd
codealxx <alok02jha@gmail.com>
2023-06-20 14:59:19 +0530

    Feat: add home page and error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
-import Footer from './components/Footer'
-import GoogleLogin from './components/GoogleLogin'
-import Spinner from './components/Spinner'
-const App = () => {
-  return (
-    <div>
-      <h1>Welcome to the Mern Invoice App</h1>
-      <Footer />
-      <GoogleLogin />
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-    </div>
-  )
-}
+
+
+import { customTheme } from "./customTheme";
+import useTitle from "./hooks/useTitle";
+
+import Footer from './components/Footer'
+import NotFound from "./components/NotFound";
+import Layout from "./components/Layout";
+
+
+import HomePage from './pages/HomePage'
+
+const App = () => {
+	useTitle("MERN Invoice - Home");
+
+	return (
+		<ThemeProvider theme={customTheme}>
+			<CssBaseline />
+			{/* {user && <Navbar />} */}
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<HomePage />} />
+
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+      
+			<Footer />
+			<ToastContainer theme="dark" />
+		</ThemeProvider>
+	);
+};
 
 export default App
--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,31 +1,89 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Button, Link, styled, Grid } from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { FaHeartBroken, FaSadTear } from "react-icons/fa";
+import error from './../images/error.png'
+
+const CreateHomeButton = styled(Button)({
+	borderColor: "black",
+	borderRadius: "25px",
+	border: "3px solid",
+	"&:hover": {
+		borderColor: "#000000",
+		boxShadow: "none",
+		border: "2px solid",
+	},
+});
 
 const NotFound = () => {
 	return (
-		<Container>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "row",
-					flexWrap: "wrap",
-					justifyContent: "center",
-					height: "94vh",
-				}}
-			>
-				<Typography
-					variant="h1"
-					align="center"
-					sx={{ fontSize: "10rem", mt: "14rem" }}
-				>
-					404 Not Found
-				</Typography>
-				<Box>
-					<FaHeartBroken className="broken-heart" />
-					<FaSadTear className="sad-tear" />
-				</Box>
-			</Box>
-		</Container>
+	<Box
+	sx={{
+	  display: 'flex',
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	  minHeight: '100vh'
+	}}
+  >
+	<Container maxWidth="md">
+	  <Grid container spacing={2} alignItems={"center"}>
+		<Grid xs={6}>
+		  <Typography variant="h1" 
+								fontFamily= "Apple Color Emoji"
+								component="div"
+								gutterBottom
+								sx={{ color: "black" }}>
+			404
+		  </Typography>
+		  <Typography variant="h6"
+								component="div"
+								gutterBottom
+								sx={{ color: "black" }}>
+			Aha! You see! You can be wrong!
+		  </Typography>
+		  <Typography variant="subtitle"
+		  						fontSize="12px"
+								component="div"
+								gutterBottom
+								sx={{ color: "grey" }}>
+			(or it could be us)....
+		  </Typography>
+		  <Typography variant="h6" 
+								component="div"
+								gutterBottom
+								sx={{ color: "black" }}>
+			....either way you should probably
+		  </Typography>
+		  <CreateHomeButton
+								variant="contained"
+								color="success"
+								size="large"
+								sx={{ fontSize: "0.5rem", borderRadius: "25px" }}
+							>
+								<Link
+									component={RouterLink}
+									to="/"
+									sx={{
+										textDecoration: "none",
+										color: "white",
+										fontSize: "0.5rem",
+									}}
+								>
+									Go Back to HOME page
+								</Link>
+		</CreateHomeButton>
+
+
+		</Grid>
+		<Grid xs={6}>
+		  <img
+			src={error}
+			alt="Error image"
+			width={500}
+		  />
+		</Grid>
+	  </Grid>
+	</Container>
+  </Box>
 	);
 };
 
