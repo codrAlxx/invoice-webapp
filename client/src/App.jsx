e0a2c6e81561c5638463c3c3cafbf3bf16efa69e
codealxx <alok02jha@gmail.com>
2023-06-20 11:38:11 +0530

    Feat: add common UI and Hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Footer from './components/Footer'
+import GoogleLogin from './components/GoogleLogin'
+import Spinner from './components/Spinner'
 const App = () => {
   return (
     <div>
       <h1>Welcome to the Mern Invoice App</h1>
       <Footer />
+      <GoogleLogin />
+
     </div>
   )
 }
