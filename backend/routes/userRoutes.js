27e5028c4816d4714e45831cb679b5ab19e95e7b
codealxx <alok02jha@gmail.com>
2023-06-20 10:16:21 +0530

    feat: Get all user profile by admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ import getUserProfile from "../controllers/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
-// import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
+import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
 import deleteUserAccount from "../controllers/user/deleteUserAccount.js";
 // import deactivateUser from "../controllers/user/deactivateUser.js";
 import role from "../middleware/roleMiddleware.js";
@@ -16,9 +16,9 @@ router
 	.patch(checkAuth, updateUserProfile)
 	.delete(checkAuth, deleteMyAccount);
 
-// router
-// 	.route("/all")
-// 	.get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
+router
+	.route("/all")
+	.get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
 
 // router
 // 	.route("/:id")
