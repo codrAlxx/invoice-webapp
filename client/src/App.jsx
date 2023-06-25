6a38c7c08c913301166765b273fe043401949c9f
codealxx <alok02jha@gmail.com>
2023-06-26 00:38:15 +0530

    Feat: Password Reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,13 +15,15 @@ import useTitle from "./hooks/useTitle";
 import Footer from './components/Footer'
 import NotFound from "./components/NotFound";
 import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
 
 
 import HomePage from './pages/HomePage'
 import RegisterPage from './features/auth/pages/RegisterPage';
 import VerifiedPage from "./features/auth/pages/VerifiedPage";
 import LoginPage from "./features/auth/pages/LoginPage";
-import Navbar from "./components/Navbar";
+import ResendEmailTokenPage from "./features/auth/pages/ResendEmailToken";
+import PasswordResetPage from "./features/auth/pages/PasswordReset";
 
 const App = () => {
 	useTitle("MERN Invoice - Home");
@@ -40,7 +42,11 @@ const App = () => {
 					<Route path="auth/verify" element={<VerifiedPage />} />
 					<Route path="login" element={<LoginPage />} />
 					<Route path="*" element={<NotFound />} />
-
+					<Route path="resend" element={<ResendEmailTokenPage />} />
+					<Route
+						path="reset_password_request"
+						element={<PasswordResetPage />}
+					/>
 				</Route>
 			</Routes>
       
